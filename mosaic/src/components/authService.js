2245216e98649bf97b02db76c79cb0262a523d86
just-stopyoon <stopjung1025@gmail.com>
2024-02-26 00:47:27 +0900

    API update (02/25)

--- mosaic/src/components/authService.js
+++ mosaic/src/components/authService.js
@@ -1,24 +1,38 @@
 // authService.js
-import axios from 'axios';
+import axios from "axios";
 
 export const idDuplicateCheck = async (id) => {
-    try {
-        const response = await axios.post('https://port-0-back-end-am952nlsys9dvi.sel5.cloudtype.app/', { id });
-        return response.data.message === '사용 가능한 아이디입니다.';
+  try {
+    const response = await axios.post(
+      "https://port-0-back-end-am952nlsys9dvi.sel5.cloudtype.app/dup",
+      { id }
+    );
+    
+    console.log("Server response:", response.data); // 서버 응답을 콘솔에 출력
+
+    if (response.data === true) {
+      // 이미 사용 중인 아이디일 때는 진행하지 않음
+      return { result: true , message: "이미 사용중인 아이디입니다." };
     }
-    catch (error) {
-        console.error(error);
-        return false;
-    }
+
+    return { result: false , message: "사용 가능한 아이디입니다." };
+  } catch (error) {
+    console.error(error);
+    return { result: false , message: "아이디가 중복되었습니다." };
+  }
 };
 
-export const signup = async (id, password) => {
-    try {
-        const response = await axios.post('https://port-0-back-end-am952nlsys9dvi.sel5.cloudtype.app/', { id, password });
-        return response.data.message === '회원가입이 완료되었습니다.';
-    }
-    catch (error) {
-        console.error(error);
-        return false;
-    }
+
+
+export const signup = async (id, pwd) => {
+  try {
+    const response = await axios.post(
+      "https://port-0-back-end-am952nlsys9dvi.sel5.cloudtype.app/signin",
+      { id, pwd }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return { result: "false", message: "서버 오류가 발생했습니다." };
+  }
 };
